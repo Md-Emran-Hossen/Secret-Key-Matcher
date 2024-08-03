42f85fd3cbc8850ce85647f6233006b8c4b77ad9
Md-Emran-Hossen <emran.cse21@gmail.com>
2024-08-03 11:19:33 +0600

    Successfully update js grid items code and make responsive for small device

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,26 +3,41 @@
 window.onload = function () {
     /* Code for generate secret key */
 
-    document.getElementById("btn-secretkey-generator").addEventListener('click', function () {
-     
-        console.log("Hello Emran");
-            let minm = 100000;
-            let maxm = 999999;
-            let randomNumber = Math.floor(Math.random() * (maxm - minm + 1)) + minm;
+     document.getElementById("btn-secretkey-generator").addEventListener('click', function () {
+        let minm = 100000;
+        let maxm = 999999;
+        let randomNumber = Math.floor(Math.random() * (maxm - minm + 1)) + minm;
 
-            let textShowSecretKey = document.getElementById("text-secretkey");
+        let textShowSecretKey = document.getElementById("text-secretkey");
 
-            textShowSecretKey.setAttribute("value",randomNumber);
-
-            // console.log(textShowSecretKey);
-            // document.getElementById("text-secretkey").innerText = randomNumber;
-
-
-            // var x = document.createElement("INPUT");
-            // x.setAttribute("type", "text");
-            // x.setAttribute("value", "Hello World!");
-            // document.body.appendChild(x);
-
+        textShowSecretKey.setAttribute("value", randomNumber);
 
     });
+
+    let gridItems = document.getElementsByClassName("grid-item");
+
+    let inputNumber = document.getElementById("text-Input");
+    let digit = 0;
+
+    for (let i = 0; i < gridItems.length - 2; i++) {
+        gridItems[i].addEventListener('click', function () {
+            display(gridItems[i].innerHTML);
+        })
+    }
+
+    function display(val) {
+        ++digit;
+        if (digit >= 0 && digit <= 6) {
+            document.getElementById("text-Input").value += val;
+        } else {
+            alert("You can't enter more then 6 digit.");
+        }
+
+    }
+
+    document.getElementById("button-clear").addEventListener('click', function () {
+        document.getElementById("text-Input").value = " ";
+        digit = 0;
+    })
+
 }
